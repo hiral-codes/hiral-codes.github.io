602c6f3fe0b9a6f5f1c419a778472a2213c4172d
hiral-codes <hiralpatel160302@gmail.com>
2024-02-09 16:30:11 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+function sidebarHandler(){
+  document.addEventListener("DOMContentLoaded", function () {
+    const menuIcon = document.querySelector("#menubar");
+    const sidebar = document.querySelector("#aside");
+    const closeBtn = document.querySelector("#close");
+    const links = document.querySelectorAll("#aside nav ul li a");
+    menuIcon.addEventListener("click", function () {
+      console.log("Menu clicked");
+      if (sidebar.style.left === "-100%" || sidebar.style.left === "") {
+        sidebar.style.left = "0";
+      } else {
+        sidebar.style.left = "-100%";
+      }
+    });
+    closeBtn.addEventListener("click", function () {
+      console.log("Close clicked");
+      sidebar.style.left = "-100%";
+    });
+    links.forEach((link) => {
+      link.addEventListener("click", function () {
+        console.log("Link clicked");
+        sidebar.style.left = "-100%";
+      });
+    });
+  });  
+}
+sidebarHandler();
